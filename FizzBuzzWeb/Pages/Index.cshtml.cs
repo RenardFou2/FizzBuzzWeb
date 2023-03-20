d1d81426c88f60265e770ef8dc86a187e51eb2f7
Renardfou <harpi473@gmail.com>
2023-03-21 00:47:18 +0100

    Zad 2 start

--- FizzBuzzWeb/Pages/Index.cshtml.cs
+++ FizzBuzzWeb/Pages/Index.cshtml.cs
@@ -1,20 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FizzBuzzWeb.Forms;
 
 namespace FizzBuzzWeb.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+
+        [BindProperty]
+        public FizzBuzzForm FizzBuzz { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+
+        public String Name { get; set; }
+
+        public void valueHandler(object sender, EventArgs e)
+        {
+            if (FizzBuzz.Number == 2)
+            {
+                MyAlert.Visible = true;
+            }
+            else
+            {
+                MyAlert.Visible = false;
+            }
+        }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
+
         public void OnGet()
         {
-
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
         }
     }
 }
